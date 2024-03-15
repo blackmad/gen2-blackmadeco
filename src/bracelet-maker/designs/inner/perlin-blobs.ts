da5b09c93e5d3f1b828732094ed36b79fd86a0e8
David Blackman <david@whizziwig.com>
2024-03-15 18:39:14 +0100

    get rid of jquery

--- src/bracelet-maker/designs/inner/perlin-blobs.ts
+++ src/bracelet-maker/designs/inner/perlin-blobs.ts
@@ -1,19 +1,17 @@
 import * as _ from 'lodash';
 import Jimp from 'jimp';
 import * as potrace from 'potrace';
-import $ from 'jquery';
 
 import { SimplexNoiseUtils } from '../../utils/simplex-noise-utils';
 import {
   OnOffMetaParameter,
   RangeMetaParameter,
-  SelectMetaParameter
+  SelectMetaParameter,
 } from '../../meta-parameter';
 import { FastAbstractInnerDesign } from './fast-abstract-inner-design';
 
 import { addToDebugLayer } from '../../utils/debug-layers';
 import { flattenArrayOfPathItems } from '../../utils/paperjs-utils';
-
 
 export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
   allowOutline = false;
@@ -30,94 +28,105 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
       chanceProbability,
       useNoiseInBitmap,
       smooth,
-      simplificationTolerance
+      simplificationTolerance,
     } = params;
 
     const imageSizeX = boundaryModel.bounds.width * scale;
     const imageSizeY = boundaryModel.bounds.height * scale;
-  
-    let image = new Jimp(imageSizeX, imageSizeY, 'black', (err, image) => {
-      if (err) throw err
+
+    const image = new Jimp(imageSizeX, imageSizeY, 'black', (err, image) => {
+      if (err) throw err;
     });
 
     console.log(image.bitmap.data.length);
 
     const self = this;
-    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
-      const chance = SimplexNoiseUtils.noise2DInRange(
-        self.simplex,
-        x / (xNoiseDivisor / (scale / 10)),
-        y / (yNoiseDivisor / (scale / 10)),
-        0,
-        1
-      );
+    image.scan(
+      0,
+      0,
+      image.bitmap.width,
+      image.bitmap.height,
+      function (x, y, idx) {
+        const chance = SimplexNoiseUtils.noise2DInRange(
+          self.simplex,
+          x / (xNoiseDivisor / (scale / 10)),
+          y / (yNoiseDivisor / (scale / 10)),
+          0,
+          1
+        );
 
-      if (chance < chanceProbability) {
+        if (chance < chanceProbability) {
           // rgba values run from 0 - 255
           if (useNoiseInBitmap) {
-            this.bitmap.data[idx + 0] = (1-chance)*255;
-            this.bitmap.data[idx + 1] = (1-chance)*255;
-            this.bitmap.data[idx + 2] = (1-chance)*255;
+            this.bitmap.data[idx + 0] = (1 - chance) * 255;
+            this.bitmap.data[idx + 1] = (1 - chance) * 255;
+            this.bitmap.data[idx + 2] = (1 - chance) * 255;
           } else {
             this.bitmap.data[idx + 0] = 255;
             this.bitmap.data[idx + 1] = 255;
             this.bitmap.data[idx + 2] = 255;
           }
-        // this.bitmap.data[idx + 1] = (1-chance)*255;
-        // this.bitmap.data[idx + 2] = (1-chance)*255;
-        // this.bitmap.data[idx + 3] = 0;
+          // this.bitmap.data[idx + 1] = (1-chance)*255;
+          // this.bitmap.data[idx + 2] = (1-chance)*255;
+          // this.bitmap.data[idx + 3] = 0;
+        }
       }
-    });
+    );
 
-    console.log(image.bitmap.data.length)
+    console.log(image.bitmap.data.length);
     const buffer = await image.getBufferAsync('image/bmp');
 
     if (params.debug) {
       image.getBase64(Jimp.MIME_JPEG, function (err, src) {
-        let img = $('#designScratchArea img');
-        console.log(img)
-        if (img.length == 0) {
-          console.log(img)
-          img = $('<img style="width:100%"/>')
-          console.log(img)
-          $('#designScratchArea').append(img[0]);
+        let img: HTMLImageElement | null = document.querySelector(
+          '#designScratchArea img'
+        );
+        console.log(img);
+        if (!img) {
+          console.log(img);
+          img = document.createElement('img');
+          img.style.width = '100%';
+          console.log(img);
+          document.querySelector('#designScratchArea')?.append(img);
         }
-        (img[0] as any).src = src;
-      })
+        img.src = src;
+      });
     }
 
-    var trace = new potrace.Potrace();
-    return await new Promise<{paths: paper.Path[]}>((resolve, reject) => {
-      trace.loadImage(buffer, function(err) {
+    const trace = new potrace.Potrace();
+    return await new Promise<{ paths: paper.Path[] }>((resolve, reject) => {
+      trace.loadImage(buffer, function (err) {
         if (err) {
           console.log('error', err);
-          reject({paths:[]});
+          reject({ paths: [] });
         }
         const svg = trace.getSVG();
-        const item = paper.project.importSVG(svg, {expandShapes:true});
-        const paths: paper.Path[] = flattenArrayOfPathItems(paper, item.children);
+        const item = paper.project.importSVG(svg, { expandShapes: true });
+        const paths: paper.Path[] = flattenArrayOfPathItems(
+          paper,
+          item.children
+        );
         item.remove();
-        item.translate(new paper.Point(
-          -item.bounds.width/2,
-          -item.bounds.height/2));
-        item.scale(1/scale, boundaryModel.bounds.center);
+        item.translate(
+          new paper.Point(-item.bounds.width / 2, -item.bounds.height / 2)
+        );
+        item.scale(1 / scale, boundaryModel.bounds.center);
 
         paths.forEach((path) => {
           path.closePath();
-  
+
           if (smooth) {
-            path.smooth({type: 'continuous'})
+            path.smooth({ type: 'continuous' });
           }
 
           if (simplificationTolerance > 0) {
-            path.simplify(simplificationTolerance/50000)
+            path.simplify(simplificationTolerance / 50000);
           }
         });
 
-        resolve({paths});
+        resolve({ paths });
       });
-    })
-
+    });
   }
 
   get designMetaParameters() {
@@ -125,7 +134,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
       new OnOffMetaParameter({
         title: 'Grayscale Algorithm',
         value: true,
-        name: 'useNoiseInBitmap'
+        name: 'useNoiseInBitmap',
       }),
       new RangeMetaParameter({
         title: 'Border Size (in)',
@@ -133,7 +142,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 0.75,
         value: 0.04,
         step: 0.01,
-        name: 'bufferWidth'
+        name: 'bufferWidth',
       }),
       new RangeMetaParameter({
         title: 'Inch to Noise Space Scale',
@@ -141,7 +150,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 250,
         step: 1,
         value: 70,
-        name: 'scale'
+        name: 'scale',
       }),
       new RangeMetaParameter({
         title: 'X Noise Divisor',
@@ -149,7 +158,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 5000.0,
         step: 1,
         value: 150,
-        name: 'xNoiseDivisor'
+        name: 'xNoiseDivisor',
       }),
       new RangeMetaParameter({
         title: 'Y Noise Divisor',
@@ -157,7 +166,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 5000.0,
         step: 1,
         value: 150,
-        name: 'yNoiseDivisor'
+        name: 'yNoiseDivisor',
       }),
       new RangeMetaParameter({
         title: 'Chance inclusion',
@@ -165,12 +174,12 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 1.0,
         value: 0.7,
         step: 0.01,
-        name: 'chanceProbability'
+        name: 'chanceProbability',
       }),
       new OnOffMetaParameter({
         title: 'Smooth blobs',
         value: false,
-        name: 'smooth'
+        name: 'smooth',
       }),
       new RangeMetaParameter({
         title: 'Simplification Tolerance',
@@ -178,7 +187,7 @@ export class InnerDesignPerlinBlobs extends FastAbstractInnerDesign {
         max: 1000,
         value: 0.0,
         step: 1,
-        name: 'simplificationTolerance'
+        name: 'simplificationTolerance',
       }),
     ];
   }
