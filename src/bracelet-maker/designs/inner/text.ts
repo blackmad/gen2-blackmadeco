5a772d45a9c4509c32135a5efb40b02f2d351c6f
David Blackman <david@whizziwig.com>
2024-03-22 08:07:43 -0600

    multiline text

--- src/bracelet-maker/designs/inner/text.ts
+++ src/bracelet-maker/designs/inner/text.ts
@@ -13,25 +13,65 @@ import { FastAbstractInnerDesign } from "./fast-abstract-inner-design";
 
 export class InnerDesignText extends FastAbstractInnerDesign {
   async makeDesign(paper: paper.PaperScope, params: any) {
-    const { boundaryModel, outerModel, fontSize, fontName, text, yOffset } =
-      params;
+    const {
+      boundaryModel,
+      outerModel,
+      fontSize,
+      fontName,
+      text,
+      yOffset,
+      lines,
+    } = params;
 
     //load a font asynchronously
     const font = await loadFont(fontName);
 
-    const textModel = new makerjs.models.Text(font, text, fontSize / 100);
+    // balance splitting text across n lines
+    const textArray = text.split(" ");
+    const textChunks = _.chunk(textArray, Math.ceil(textArray.length / lines));
+    const textLines = textChunks.map((line) => line.join(" "));
 
-    const svg = makerjs.exporter.toSVG(textModel);
+    let paths = [];
 
-    const importedItem = new paper.Item().importSVG(svg);
-    // importedItem.fitBounds(boundaryModel.bounds, false);
-    importedItem.translate(
-      boundaryModel.bounds.center
-        .subtract(importedItem.bounds.center)
-        .add(new paper.Point(0, yOffset))
+    const maxLineHeight = Math.max(
+      ...textLines.map((line, i) => {
+        const textModel = new makerjs.models.Text(font, line, fontSize / 100);
+
+        const svg = makerjs.exporter.toSVG(textModel);
+
+        const importedItem = new paper.Item().importSVG(svg);
+        return importedItem.bounds.height;
+      })
     );
 
-    const paths = flattenArrayOfPathItems(paper, [importedItem]);
+    let accumulatedYOffset =
+      yOffset + (boundaryModel.bounds.height - maxLineHeight * lines) / 2;
+
+    textLines.forEach((line, i) => {
+      const textModel = new makerjs.models.Text(font, line, fontSize / 100);
+
+      const svg = makerjs.exporter.toSVG(textModel);
+
+      const importedItem = new paper.Item().importSVG(svg);
+      // importedItem.fitBounds(boundaryModel.bounds, false);
+      console.log(
+        importedItem.bounds.center,
+        importedItem.bounds.height,
+        accumulatedYOffset
+      );
+      importedItem.translate(
+        [
+          boundaryModel.bounds.center.x - importedItem.bounds.center.x,
+          accumulatedYOffset,
+        ]
+        // boundaryModel.bounds.center
+        //   .subtract(importedItem.bounds.center)
+        //   .add(new paper.Point(0, accumulatedYOffset))
+      );
+
+      accumulatedYOffset += maxLineHeight;
+      paths = [...paths, ...flattenArrayOfPathItems(paper, [importedItem])];
+    });
 
     return Promise.resolve({ paths });
   }
@@ -43,7 +83,7 @@ export class InnerDesignText extends FastAbstractInnerDesign {
         min: 0,
         max: 500,
         step: 1,
-        value: 200,
+        value: 100,
         name: "fontSize",
       }),
       new SelectMetaParameter({
@@ -65,6 +105,14 @@ export class InnerDesignText extends FastAbstractInnerDesign {
         step: 0.1,
         value: 0,
         name: "yOffset",
+      }),
+      new RangeMetaParameter({
+        title: "Lines",
+        min: 1,
+        max: 10,
+        step: 1,
+        value: 1,
+        name: "lines",
       }),
     ];
   }
