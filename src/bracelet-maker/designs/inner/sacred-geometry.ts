d7d81c3f818184421c7e8599d67a6feced067032
David Blackman <david@whizziwig.com>
2024-03-23 08:05:28 -0600

    sacred geometry

--- src/bracelet-maker/designs/inner/sacred-geometry.ts
+++ src/bracelet-maker/designs/inner/sacred-geometry.ts
@@ -1,7 +1,9 @@
-import { AbstractExpandInnerDesign } from "./abstract-expand-and-subtract-inner-design";
+import { displayDataUriImageToConsole } from "../../utils/debug-utils";
+import { getEvenlySpacePointsAlongPath } from "../../utils/paperjs-utils";
+import { FastAbstractInnerDesign } from "./fast-abstract-inner-design";
 
-export class InnerDesignSacredGeometry extends AbstractExpandInnerDesign {
-  async makePaths(paper: paper.PaperScope, params: any) {
+export class InnerDesignSacredGeometry extends FastAbstractInnerDesign {
+  async makeDesign(paper: paper.PaperScope, params: any) {
     const boundaryModel: paper.Path = params.boundaryModel;
 
     const points = getEvenlySpacePointsAlongPath({
@@ -9,14 +11,45 @@ export class InnerDesignSacredGeometry extends AbstractExpandInnerDesign {
       numPoints: 10,
     });
 
-    const paths: paper.Point[][] = [];
+    const lines: paper.Path.Line[] = [];
     points.forEach((p1, i) => {
       points.slice(i + 1).forEach((p2, j) => {
-        paths.push([p1, p2]);
+        const line = new paper.Path.Line(p1, p2);
+        line.strokeWidth = 0.03;
+        line.strokeColor = "black";
+        lines.push(line);
       });
     });
 
-    return paths;
+    // const box = new paper.Path.Rectangle(new paper.Rectangle(0, 0, 10, 10));
+    // box.strokeColor = "black";
+    // box.strokeWidth = 0.03;
+    // box.fillColor = "red";
+    // box.visible = true;
+    // lines.push(box);
+
+    // const totalScene = new paper.CompoundPath(lines);
+    // const totalSceneRaster = box.rasterize();
+    // displayDataUriImageToConsole(totalSceneRaster.toDataURL());
+
+    const box = new paper.Path.Rectangle(
+      new paper.Rectangle(
+        0,
+        0,
+        boundaryModel.bounds.width,
+        boundaryModel.bounds.height
+      )
+    );
+    box.strokeColor = "black";
+    box.strokeWidth = 0.03;
+    box.fillColor = "red";
+
+    const raster = box.rasterize();
+    displayDataUriImageToConsole(raster.toDataURL());
+
+    console.log(raster.toDataURL());
+
+    return { paths: [box] };
   }
 
   get designMetaParameters() {
--- src/bracelet-maker/utils/debug-utils.ts
+++ src/bracelet-maker/utils/debug-utils.ts
@@ -13,3 +13,41 @@ export function addDebugInfo(s: string) {
     }
   }
 }
+
+export function displayDataUriImageToConsole(dataUri: string) {
+  // console.log(dataUri);
+  // const height = 100;
+  // console.log(
+  //   "%c",
+  //   [
+  //     "font-size: 1px;",
+  //     `line-height: ${height}px;`,
+  //     // `padding: ${height * 0.5}px ${width * 0.5}px;`,
+  //     // `background-size: ${this.width}px ${this.height}px;`,
+  //     `background: url(${dataUri});`,
+  //   ].join(" ")
+  // );
+
+  console.log(dataUri);
+
+  const º = "%c";
+  const consoleNormal = "font-family: sans-serif";
+  const consoleBold = "font-family: sans-serif;" + "font-weight: bold";
+  const consoleCode =
+    "background: #EEEEF6;" +
+    "border: 1px solid #B2B0C1;" +
+    "border-radius: 7px;" +
+    "padding: 2px 8px 3px;" +
+    "color: #5F5F5F;" +
+    "line-height: 22px;" +
+    "box-shadow: 0px 0px 1px 1px rgba(178,176,193,0.3)";
+  const consoleBackground = `
+  background-image: url('${dataUri}');
+  width: 100px;
+  height: 100px;
+  padding: 100px;
+  line-height: 100px;
+  `;
+
+  console.info(º + " ", consoleBackground);
+}
